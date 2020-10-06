f8267626d353fbff3a111e6ce7f9f3e91fc11d98
Mr.Thirachai Sueapo <mandmod@gmail.com>
2020-10-06 14:35:22 +0700

    test Print
    ทดสอบการ ใช้คำสั่ง Print

--- main.py
+++ main.py
@@ -9,8 +9,21 @@ print('Hello My name is Mod from Chonburi22')
 print('Hello My name is Mod from Chonburi22')
 
 
-
+'''todo สวดัดีครับ
 print('Hello My name is Mod from Chonburi33')
+'''
+
+"""todo สวัดดีครับ
+print('Hello My name is Mod UI-Desing')
+"""
+
 
 print('Hello My name is Mod UI-Desing')
-print('Hello My name is Mod UI-Desing')
+
+print(10+10+1*5)
+
+print((10+10+1)*5)
+
+
+
+#todo สวัดดีครับ
